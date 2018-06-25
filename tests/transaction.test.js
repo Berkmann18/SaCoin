1bdb90c00475eb4b50de17004c5d0886c8c6ed2b
Berkmann18 <maxieberkmann@gmail.com>
2018-06-25 01:46:17 +0100

    More tests and fixes on Transaction

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -2,8 +2,9 @@ const Transaction = require('../src/transaction'), {genKey, sign, verify} = requ
 
 let fromKP = genKey(), toKP = genKey(), from = sign(fromKP.sk, 'from'), to = sign(toKP.sk, 'to');
 
+const amt = 10, trans = new Transaction(fromKP.pk, toKP.pk, amt, from);
+
 test('Init', () => {
-  let amt = 10, trans = new Transaction(fromKP.pk, toKP.pk, amt, from);
   expect(trans.fromPubKey).toBe(fromKP.pk);
   expect(trans.toPubKey).toBe(toKP.pk);
   expect(trans.amount).toBe(amt);
@@ -11,8 +12,21 @@ test('Init', () => {
   expect(trans.fee).toBe(FEE);
   expect(trans.timestamp <= Date.now()).toBeTruthy();
   expect(trans.isValid()).toBeTruthy(); //tests calculateHash() so no need to test that
-  // expect(trans.hasValidSignature()).toBeTruthy();
-  expect(verify(fromKP.pk, trans.hash, trans.signature)).toBeFalsy(); //Should be true (n.b: the trans.hash shouldn't be dependent on the signature)
-  expect(verify(fromKP.pk, 'from', trans.signature)).toBeTruthy(); //It means that the message used in the signature should be something else?!
   expect(trans.toString()).toBe(`Transaction(fromPubKey=${trans.fromPubKey}, toPubKey=${trans.toPubKey}, amount=${amt}, timestamp=${trans.timestamp}, fee=${FEE}, hash=${trans.hash})`)
+});
+
+test('Signatures et al', () => {
+  expect.assertions(4);
+  return new Promise((resolve) => {
+    resolve(trans.hasValidSignature());
+  }).then(bool => {
+    expect(bool).toBeFalsy();
+    return expect(verify(fromKP.pk, trans.hash, trans.signature)).toBeFalsy()
+  })
+    .then(wrong => trans.sign(fromKP.sk))
+    .then(signed => expect(trans.hasValidSignature()).toBeTruthy())
+    .then(correct => {
+      expect(verify(fromKP.pk, trans.hash, trans.signature)).toBeTruthy();
+    })sr
+    .catch(err => console.log('This went wrong:', err))
 });
