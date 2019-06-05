c5517eeaf38eb96a41e00cb55ca4e3b372c3091d
Berkmann18 <maxieberkmann@gmail.com>
2019-06-05 17:46:04 +0100

    Update: Added `mine` [WIP]

--- src/cli.js
+++ src/cli.js
@@ -7,6 +7,7 @@
 const vorpal = require('vorpal')();
 const join = require('./commands/join');
 const inf = require('./commands/info');
+const mine = require('./commands/mine');
 const Blockchain = require('./blockchain');
 
 const SXC = new Blockchain();
@@ -16,14 +17,8 @@ const user = {
 };
 
 // @todo Implement: mining, sending transactions, checking balance, discovering peers
-// vorpal
-//   .command('mine', 'Mine a block')
-//   .action((args, cb) => {
-//     throw new Error('Not implemented yet')
-//     // cb();
-//   })
 vorpal.command('join', 'Join the network').action(join(vorpal, SXC));
-
 vorpal.command('info', 'User information').action(inf(user, SXC));
+vorpal.command('mine', 'Mine a block').action(mine(user, SXC));
 
 vorpal.delimiter('sacoin$').show();
