2b694654a116736a75c063d48355189db32beca9
Berkmann18 <maxieberkmann@gmail.com>
2019-06-05 15:58:56 +0100

    Chore: Refactored `pwcheck`

--- src/pwcheck/index.js
+++ src/pwcheck/index.js
@@ -1,6 +1,7 @@
 'use strict';
-const {rev} = require('./utils');
 const getComplexity = require('./complex');
+const patternMatch = require('./patternMatch');
+const seqStrPatterns = require('./seqStrPatterns');
 /* istanbul ignore */
 
 /**
@@ -10,25 +11,6 @@ const getComplexity = require('./complex');
  * @returns {{score: number: complexity: string}} Score (in percentage) with(out) the complexity level
  */
 const checkPassword = password => {
-  let score = 0,
-    uppercase = 0,
-    lowercase = 0,
-    num = 0,
-    symbol = 0,
-    midChar = 0,
-    uniqueChar = 0,
-    repChar = 0,
-    repInc = 0,
-    consecUppercase = 0,
-    consecLowercase = 0,
-    consecNum = 0,
-    seqAlpha = 0,
-    seqNum = 0,
-    seqSymbol = 0,
-    reqChar = 0;
-  let tmpUppercase = '',
-    tmpLowercase = '',
-    tmpNum = '';
   const MULT_MID_CHAR = 2,
     MULT_CONSEC_UPPERCASE = 2,
     MULT_CONSEC_LOWERCASE = 2,
@@ -39,78 +21,25 @@ const checkPassword = password => {
     MULT_LEN = 4,
     MULT_NUM = 4,
     N_MULT_SYM = 6,
-    MIN_PW_LEN = 8,
-    ALPHA = 'abcdefghijklmnopqrstuvwxyz',
-    NUMBERS = '01234567890',
-    SYMBOLS = ')!@#$%^&*(';
-  score = (password.length * MULT_LEN) | 0;
-  const pwArr = password.replace(/\s+/g, '').split(/\s*/);
+    MIN_PW_LEN = 8;
+  let reqChar = 0,
+    score = (password.length * MULT_LEN) | 0;
 
   /* Loop through password to check for Symbol, Numeric, Lowercase and Uppercase pattern matches */
-  for (let i = 0; i < pwArr.length; i++) {
-    if (pwArr[i].match(/[A-Z]/g)) {
-      if (tmpUppercase !== '' && tmpUppercase + 1 === i) consecUppercase++;
-      tmpUppercase = i;
-      uppercase++;
-    } else if (pwArr[i].match(/[a-z]/g)) {
-      if (tmpLowercase !== '' && tmpLowercase + 1 === i) consecLowercase++;
-      tmpLowercase = i;
-      lowercase++;
-    } else if (pwArr[i].match(/\d/g)) {
-      if (i > 0 && i < pwArr.length - 1) midChar++;
-      if (tmpNum !== '' && tmpNum + 1 === i) consecNum++;
-      tmpNum = i;
-      num++;
-    } else if (pwArr[i].match(/[^a-zA-Z0-9_]/g)) {
-      if (i > 0 && i < pwArr.length - 1) midChar++;
-      symbol++;
-    }
-    //Repetition check
-    let bCharExists = false;
-    for (let j = 0; j < pwArr.length; j++) {
-      if (pwArr[i] === pwArr[j] && i != j) {
-        //Repetition present
-        bCharExists = true;
-        /*
-       Calculate increment deduction based on proximity to identical characters
-       Deduction is incremented each time a new match is discovered
-       Deduction amount is based on total password length divided by the
-       difference of distance between currently selected match
-       */
-        repInc += Math.abs(pwArr.length / (j - i));
-      }
-    }
-    if (bCharExists) {
-      repChar++;
-      uniqueChar = pwArr.length - repChar;
-      repInc = (uniqueChar ? repInc / uniqueChar : repInc) | 1;
-    }
-  }
+  const {
+    consecUppercase,
+    consecLowercase,
+    num,
+    uppercase,
+    lowercase,
+    symbol,
+    midChar,
+    consecNum,
+    repChar,
+    repInc
+  } = patternMatch(password);
 
-  let s = 0,
-    sFwd,
-    sRev;
-  const pw = password.toLowerCase();
-  //Check for sequential alpha string patterns (forward and reverse)
-  for (; s < 23; s++) {
-    sFwd = ALPHA.substring(s, s + 3);
-    sRev = rev(sFwd);
-    if (pw.includes(sFwd) || pw.includes(sRev)) seqAlpha++;
-  }
-
-  //Check for sequential numeric string patterns (forward and reverse)
-  for (s = 0; s < 8; s++) {
-    sFwd = NUMBERS.substring(s, s + 3);
-    sRev = rev(sFwd);
-    if (pw.includes(sFwd) || pw.includes(sRev)) seqNum++;
-  }
-
-  //Check for sequential symbol string patterns (forward and reverse)
-  for (s = 0; s < 8; s++) {
-    sFwd = SYMBOLS.substring(s, s + 3);
-    sRev = rev(sFwd);
-    if (pw.includes(sFwd) || pw.includes(sRev)) seqSymbol++;
-  }
+  const {seqAlpha, seqNum, seqSymbol} = seqStrPatterns(password);
 
   //Modify overall score value based on usage vs requirements
   //General point assignment
@@ -131,7 +60,7 @@ const checkPassword = password => {
   if (seqNum > 0) score -= seqNum * MULT_SEQ_NUM; //Sequential numeric strings exist (3 characters or more)
   if (seqSymbol > 0) score -= seqSymbol * MULT_SEQ_SYM; //Sequential symbol strings exist (3 characters or more)
 
-  //Determine if mandatory requirements have been met and set image indicators accordingly
+  //Determine if mandatory requirements have been met
   const arrChars = [password.length, uppercase, lowercase, num, symbol];
   if (password.length >= MIN_PW_LEN) reqChar++;
   for (let c = 0; c < arrChars.length; c++) {
---
+++ src/pwcheck/seqStrPatterns.js
@@ -0,0 +1,40 @@
+const {rev} = require('./utils');
+
+const ALPHA = 'abcdefghijklmnopqrstuvwxyz',
+  NUMBERS = '01234567890',
+  SYMBOLS = ')!@#$%^&*(';
+
+const seqStrPatterns = password => {
+  let s = 0,
+    seqAlpha = 0,
+    seqNum = 0,
+    seqSymbol = 0,
+    sFwd,
+    sRev;
+  const pw = password.toLowerCase();
+  //Check for sequential alpha string patterns (forward and reverse)
+  for (; s < 23; s++) {
+    sFwd = ALPHA.substring(s, s + 3);
+    sRev = rev(sFwd);
+    if (pw.includes(sFwd) || pw.includes(sRev)) seqAlpha++;
+  }
+
+  /* @TODO Loop-fuse both loops below */
+  //Check for sequential numeric string patterns (forward and reverse)
+  for (s = 0; s < 8; s++) {
+    sFwd = NUMBERS.substring(s, s + 3);
+    sRev = rev(sFwd);
+    if (pw.includes(sFwd) || pw.includes(sRev)) seqNum++;
+  }
+
+  //Check for sequential symbol string patterns (forward and reverse)
+  for (s = 0; s < 8; s++) {
+    sFwd = SYMBOLS.substring(s, s + 3);
+    sRev = rev(sFwd);
+    if (pw.includes(sFwd) || pw.includes(sRev)) seqSymbol++;
+  }
+
+  return {seqAlpha, seqNum, seqSymbol};
+};
+
+module.exports = seqStrPatterns;
